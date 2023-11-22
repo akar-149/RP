1f590f5a78abdda594fee9dd2fbaf118bc950903
Akar Jan <akar.cc99@gmail.com>
2023-11-22 17:21:18 +0300

    I added a label to Form1,which carries "Hello World" text.

--- test/Form1.Designer.cs
+++ test/Form1.Designer.cs
@@ -29,13 +29,35 @@ namespace test
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 38.25F);
+            this.label1.Location = new System.Drawing.Point(247, 189);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(286, 59);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hello World";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
 
